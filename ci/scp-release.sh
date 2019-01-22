85c11ba1e8abd588c73dd72b8030118a4a5bd135
Artur Dryomov <artur.dryomov@gmail.com>
2019-01-22 12:50:31 +0300

    Address review.

--- ci/scp-release.sh
+++ ci/scp-release.sh
@@ -3,24 +3,24 @@ set -eu
 
 # Expected environment variables:
 # TRAVIS_REPO_SLUG, TRAVIS_TAG
-# STORAGE_USER, STORAGE_HOST, STORAGE_PATH
+# STORAGE_USER, STORAGE_PRIVATE_KEY, STORAGE_HOST, STORAGE_PATH
 
 RELEASE_FILE_CONTENT_TYPE="audio/mpeg"
+STORAGE_PRIVATE_KEY_FILE="storage.key"
 
 # Use GitHub API to get the release file URL associated with a Git tag.
 
-RELEASES="$(curl \
+RELEASE="$(curl \
   --silent \
+  --fail \
   --show-error \
   --request GET \
   --header "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/${GITHUB_SLUG}/releases"
+  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases/tags/${TRAVIS_TAG}"
 )"
 
 RELEASE_FILE="$(
-  echo "${RELEASES}" | jq --raw-output "
-    .[] |
-    select(.tag_name == \"${GITHUB_TAG}\") |
+  echo "${RELEASE}" | jq --raw-output "
     .assets |
     .[] |
     select(.content_type = \"${RELEASE_FILE_CONTENT_TYPE}\")
@@ -34,10 +34,13 @@ RELEASE_FILE_NAME="$(echo "${RELEASE_FILE}" | jq --raw-output ".name")"
 
 curl \
   --silent \
+  --fail \
   --show-error \
   --location "${RELEASE_FILE_URL}" \
   --output "${RELEASE_FILE_NAME}"
 
 # Upload the release file to the remote location.
 
-scp "${RELEASE_FILE_NAME}" ${STORAGE_USER}@${STORAGE_HOST}:${STORAGE_PATH}
+echo "${STORAGE_PRIVATE_KEY}" >> "${STORAGE_PRIVATE_KEY_FILE}"
+scp "${RELEASE_FILE_NAME}" ${STORAGE_USER}@${STORAGE_HOST}:${STORAGE_PATH} -i "${STORAGE_PRIVATE_KEY_FILE}"
+rm "${STORAGE_PRIVATE_KEY_FILE}"
